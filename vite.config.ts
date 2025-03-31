1cd865e5491ec973854f38cea5a08b34e0722956
404RichardNotFound <onlydrae48@gmail.com>
2025-03-31 08:13:14 +0000

    fix ag-grid display issues

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    cssCodeSplit: false, // Prevents splitting CSS files
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
